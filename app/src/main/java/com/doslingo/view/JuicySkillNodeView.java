5c6839f0f0936c37cb772560b9bec6bac35c8361
Alon Albert <alon.albert@gmail.com>
2019-04-25 17:40:01 +0300

    Icon & Background

--- app/src/main/java/com/doslingo/view/JuicySkillNodeView.java
+++ app/src/main/java/com/doslingo/view/JuicySkillNodeView.java
@@ -1,6 +1,7 @@
 package com.doslingo.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 
@@ -9,6 +10,9 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import com.doslingo.R;
 
 public class JuicySkillNodeView extends ConstraintLayout {
+
+  private JuicyFillingRingView progressRing;
+
   public JuicySkillNodeView(Context context) {
     super(context);
     init(context, null, 0);
@@ -27,5 +31,13 @@ public class JuicySkillNodeView extends ConstraintLayout {
   private void init(Context context, AttributeSet attrs, int defStyleAttr) {
     LayoutInflater.from(context).inflate(R.layout.view_skill_node_juicy, this, true);
 
+    progressRing = findViewById(R.id.progressRing);
+    final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.JuicySkillNodeView, defStyleAttr, 0);
+    try {
+      progressRing.setProgress(a.getFloat(R.styleable.JuicySkillNodeView_progress, 0));
+    } finally {
+      a.recycle();
+    }
   }
+
 }
