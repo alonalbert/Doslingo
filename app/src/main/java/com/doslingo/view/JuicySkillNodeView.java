60ac28f52aa8561ab2c03b78c800b24b13d7f9b9
Alon Albert <alon.albert@gmail.com>
2019-04-25 19:03:31 +0300

    Icon & Background attributes and some cleanup

--- app/src/main/java/com/doslingo/view/JuicySkillNodeView.java
+++ app/src/main/java/com/doslingo/view/JuicySkillNodeView.java
@@ -5,39 +5,78 @@ import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.doslingo.R;
 
 public class JuicySkillNodeView extends ConstraintLayout {
+  private static final int[] LEVEL_BACKGROUNDS = new int[]{
+      R.drawable.skill_icon_background_locked,
+      R.drawable.skill_icon_background_beetle,
+      R.drawable.skill_icon_background_macaw,
+      R.drawable.skill_icon_background_owl,
+      R.drawable.skill_icon_background_cardinal,
+      R.drawable.skill_icon_background_fox,
+      R.drawable.skill_icon_background_mastery,
+  };
+  private static final String[] LEVEL_ICON_PREFIX = new String[]{
+      "levelslocked",
+      "purple",
+      "blue",
+      "green",
+      "red",
+      "orange",
+      "levelsgold",
+  };
 
-  private JuicyFillingRingView progressRing;
+  private final JuicyFillingRingView progressRing;
+  private final AppCompatImageView icon;
+
+  private int level;
+  private int skill;
 
   public JuicySkillNodeView(Context context) {
-    super(context);
-    init(context, null, 0);
+    this(context, null);
   }
 
   public JuicySkillNodeView(Context context, AttributeSet attrs) {
-    super(context, attrs);
-    init(context, attrs, 0);
+    this(context, attrs, 0);
   }
 
   public JuicySkillNodeView(Context context, AttributeSet attrs, int defStyleAttr) {
     super(context, attrs, defStyleAttr);
-    init(context, attrs, defStyleAttr);
-  }
-
-  private void init(Context context, AttributeSet attrs, int defStyleAttr) {
     LayoutInflater.from(context).inflate(R.layout.view_skill_node_juicy, this, true);
 
     progressRing = findViewById(R.id.progressRing);
+    icon = findViewById(R.id.icon);
     final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.JuicySkillNodeView, defStyleAttr, 0);
     try {
-      progressRing.setProgress(a.getFloat(R.styleable.JuicySkillNodeView_progress, 0));
+      level = a.getInteger(R.styleable.JuicySkillNodeView_level, 0);
+      skill = a.getInteger(R.styleable.JuicySkillNodeView_skill, 1);
+      setProgress(a.getFloat(R.styleable.JuicySkillNodeView_progress, 0));
     } finally {
       a.recycle();
     }
+    update();
   }
 
+  private void setSkill(int skill) {
+    this.skill = skill;
+    update();
+  }
+
+  private void setLevel(int level) {
+    this.level = level;
+    update();
+  }
+
+  private void update() {
+    icon.setBackgroundResource(LEVEL_BACKGROUNDS[this.level]);
+    icon.setImageResource(getResources().getIdentifier("icon_" + LEVEL_ICON_PREFIX[level] + "_" + skill, "drawable", getContext().getPackageName()));
+  }
+
+  public void setProgress(float progress) {
+    progressRing.setProgress(progress);
+  }
 }
